22a50743bf36c23036586e79e8f601ed568c57db
vsbdn <v.boldurean@gmail.com>
2021-02-24 14:38:57 +0000

    fix HexletFs/tests

--- __tests__/hexletfs.test.js
+++ __tests__/hexletfs.test.js
@@ -1,3 +1,4 @@
+// @ts-check
 import HexletFs from '../src/HexletFs.js';
 
 describe('FS', () => {
@@ -5,32 +6,48 @@ describe('FS', () => {
 
   beforeEach(() => {
     files = new HexletFs();
-  });
-
-  it('#mkdirSync', () => {
     files.mkdirSync('/etc');
-    expect(files.statSync('/etc').isDirectory()).toBeTruthy();
-
+    files.mkdirSync('/opt');
     files.mkdirSync('/etc/nginx');
-    expect(files.statSync('/etc/nginx').isDirectory()).toBeTruthy();
   });
 
-  it('#mkdirSync2', () => {
-    files.mkdirSync('/var/');
-    expect(files.statSync('/var////').isDirectory()).toBeTruthy();
-    expect(files.statSync('/var').isDirectory()).toBeTruthy();
+  it('hexletFs', () => {
+    expect(files.mkdirSync('/opt/folder/inner')).toBe(false);
+    expect(files.statSync('/opt').isDirectory()).toBe(true);
+    expect(files.statSync('/etc/unknown')).toBe(null);
 
-    files.mkdirSync('/var//log//////');
-    expect(files.statSync('/var/log').isDirectory()).toBeTruthy();
-    expect(files.statSync('/var///log').isDirectory()).toBeTruthy();
-  });
+    files.touchSync('/opt/file.txt');
+    files.touchSync('/etc/nginx/nginx.conf');
+    expect(files.statSync('/etc/nginx').isDirectory()).toBe(true);
+    expect(files.statSync('/etc/nginx').isFile()).toBe(false);
+    expect(files.statSync('/etc/nginx/nginx.conf').isDirectory()).toBe(false);
+    expect(files.statSync('/etc/nginx/nginx.conf').isFile()).toBe(true);
+    expect(files.mkdirSync('/etc/nginx/nginx.conf/wrong')).toBe(false);
 
-  it('#touchSync', () => {
-    files.touchSync('/file.txt');
-    expect(files.statSync('/file.txt').isFile()).toBeTruthy();
+    expect(files.touchSync('/etc/nginx/nginx.conf/wrong')).toBe(false);
+    expect(files.touchSync('/opt/folder/inner')).toBe(false);
+    expect(files.statSync('/opt/file.txt').isFile()).toBe(true);
 
-    files.mkdirSync('/etc');
-    files.touchSync('/etc/bashrc');
-    expect(files.statSync('/etc/bashrc').isFile()).toBeTruthy();
+    files.mkdirpSync('/etc/nginx/conf.d');
+    files.mkdirpSync('/usr/admin/docs');
+    expect(files.readdirSync('/usr/admin/docs')).toEqual([]);
+    expect(files.statSync('/etc/nginx/conf.d').isDirectory()).toBe(true);
+    expect(files.mkdirpSync('/etc/nginx/nginx.conf/wrong')).toBe(false);
+
+    expect(files.readdirSync('/etc/nginx')).toEqual(['nginx.conf', 'conf.d']);
+    expect(files.readdirSync('/')).toEqual(['etc', 'opt', 'usr']);
+    expect(files.readdirSync('/etc/nginx/undefined')).toBe(false);
+    expect(files.readdirSync('/etc/nginx/nginx.conf')).toBe(false);
+
+    files.rmdirSync('/etc/nginx/conf.d');
+    expect(files.readdirSync('/etc/nginx')).toEqual(['nginx.conf']);
+
+    expect(files.rmdirSync('/etc/unknown')).toBe(false);
+    expect(files.rmdirSync('/etc/nginx')).toBe(false);
+
+    expect(files.rmdirSync('/etc/nginx/nginx.conf')).toBe(false);
+
+    files.rmdirSync('/usr/admin/docs');
+    expect(files.readdirSync('/usr/admin/docs')).toBe(false);
   });
 });
--- src/HexletFs.js
+++ src/HexletFs.js
@@ -1,29 +1,80 @@
 import path from 'path';
 import Tree from '@hexlet/trees';
-
-import Dir from './Dir.js';
-import File from './File.js';
+import { Dir, File } from '@hexlet/fs';
 
 const getPathParts = (filepath) => filepath.split(path.sep).filter((part) => part !== '');
 
-export default class {
+export default class HexletFs {
   constructor() {
     this.tree = new Tree('/', new Dir('/'));
   }
 
   statSync(filepath) {
     const current = this.findNode(filepath);
+    if (!current) {
+      return null;
+    }
     return current.getMeta().getStats();
   }
 
-  touchSync(filepath) {
-    const { dir, base } = path.parse(filepath);
-    return this.findNode(dir).addChild(base, new File(base));
+  mkdirSync(filepath) {
+    const current = this.findNode(filepath);
+    if (current) {
+      return false;
+    }
+    const { base, dir } = path.parse(filepath);
+    const parent = this.findNode(dir);
+    if (!parent || !parent.getMeta().isDirectory()) {
+      return false;
+    }
+    parent.addChild(base, new Dir(base));
+    return true;
   }
 
-  mkdirSync(filepath) {
-    const { dir, base } = path.parse(filepath);
-    return this.findNode(dir).addChild(base, new Dir(base));
+  mkdirpSync(filepath) {
+    const current = this.findNode(filepath);
+    if (!current) {
+      const { dir } = path.parse(filepath);
+      this.mkdirpSync(dir);
+    } else if (!current.getMeta().isDirectory()) {
+      return false;
+    }
+    return this.mkdirSync(filepath);
+  }
+
+  touchSync(filepath) {
+    const { base, dir } = path.parse(filepath);
+    const parent = this.findNode(dir);
+    if (!parent) {
+      return false;
+    }
+    if (!parent.getMeta().isDirectory()) {
+      return false;
+    }
+    parent.addChild(base, new File(base, ''));
+    return true;
+  }
+
+  rmdirSync(filepath) {
+    const { base } = path.parse(filepath);
+    const current = this.findNode(filepath);
+    if (!current) {
+      return false;
+    }
+    if (!current.getMeta().isDirectory() || current.hasChildren()) {
+      return false;
+    }
+    current.getParent().removeChild(base);
+    return true;
+  }
+
+  readdirSync(filepath) {
+    const current = this.findNode(filepath);
+    if (!current || !current.getMeta().isDirectory()) {
+      return false;
+    }
+    return current.getChildren()
+      .map((child) => child.getKey());
   }
 
   findNode(filepath) {
